7cafbccc059a42e82b789f5a6d3a3a9b4b28d89b
Alasdair Morrison <amorri40@gmail.com>
2013-05-13 14:53:38 +0100

    Added support for asking multiple questions per set as defined in the settings file

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def format_question_answer (title_without_spaces, card_no, text, question_or_ans
     html+="<div><a name='"+title_without_spaces+"_"+question_or_answer+"_"+str(card_no)+"'></a>"
     if image != None:
         html+="<img src='"+image+"'></img></br>"
-    html+="<p><b>"+str(card_no)+")</b> "+text+"</p>"
+    html+="<p><b>"+str(card_no+1)+")</b> "+text+"</p>"
     html+=" <a href='#"+title_without_spaces+"_"+opposite+"_"+str(card_no)+"'>("+opposite+")</a></div></br>"
     return html
 
@@ -26,24 +26,24 @@ for set_id in flashcard_settings.sets_to_ask:
     description = set_json['description']
     has_image = set_json['has_image']
 
-    print set_json
+    print "Set: "+title
     message_body+="<div style='border: 2px solid; border-radius: 2px; padding: 10px;'><h1>"+title+"</h1>"+description+"</br></br><h2>Questions</h2>"
 
     all_cards=set_json['cards']
     no_of_cards = len(all_cards)
     answers_html = "<h2>Answers</h2>"
 
-    card_split_minimum=0 #used to split the deck into the number of questions wanted, then get a random value from that half of the deck
-    card_split_maximum=0
+    previous_card_numbers=[]
+    card_no=-1
 
     number_of_questions_to_ask=flashcard_settings.number_to_ask_per_set
     for current_question_number in range (0,number_of_questions_to_ask):
-        card_split_minimum=card_split_maximum
-        card_split_maximum=(current_question_number+1)*(no_of_cards/number_of_questions_to_ask)-1
-        card_no = randint(card_split_minimum,card_split_maximum)
+        previous_card_numbers.append(card_no)
+        while card_no in previous_card_numbers: card_no=randint(0,no_of_cards-1)
+        print "card_no:"+str(card_no)
         this_card = all_cards[card_no]
         question = this_card['front']
-        answer = this_card['back']
+        answer = this_card['back'].replace(">","&gt;").replace("<","&lt;")
         question_image=this_card['image_front']
         answer_image=this_card['image_url']
         message_body+=format_question_answer(title_without_spaces, current_question_number, question, "Question", "Answer",question_image)
@@ -58,10 +58,9 @@ msg['Subject'] = 'Daily Flashcards'
 msg['From'] = flashcard_settings.email
 msg['Reply-to'] = flashcard_settings.email
 msg['To'] = flashcard_settings.email
-part1 = MIMEText('your email is so plain', 'plain')
-part2 = MIMEText(message_body, 'html')
-#msg.attach(part1)
-msg.attach(part2)
+
+htmlpart = MIMEText(message_body, 'html')
+msg.attach(htmlpart)
 
 server = smtplib.SMTP('smtp.gmail.com',587) #port 465 or 587
 server.ehlo()
